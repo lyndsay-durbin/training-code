e7d615b3afbbfc62a3b426c986bb7b66da0db688
lyndsay durbin <lyndsaydurbin@yahoo.com>
2019-10-24 15:33:13 -0500

    add HelloWorld project

---
+++ 00_csharp/HelloWorld/HelloWorld.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using sc = System.Console; //this is an import with an alias
+
+namespace HelloWorld.Client 
+{
+   /// <summary>
+   /// this is an xml comment!!! these are used to let other assemblies know what these things do
+   /// classes and methods should have xml comments going forward
+   /// what is the functionality of the method???? what it does not what it is
+   /// 
+   /// contains the start of the program
+   /// </summary>
+    internal class Program
+    {
+       /// <summary>
+       /// starts the program
+       /// </summary>
+        private static void Main()
+        {
+            //sc.WriteLine("Hello World!");
+            //sc.WriteLine("don't write comments! your code should be clear enough without comments");
+            //sc.WriteLine("Hello Baby!");
+            sc.WriteLine("How you doin'?");
+        }
+    }
+}
